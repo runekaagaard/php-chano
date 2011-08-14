50c931f21a0be5d3a36766cfcdcc75f3f632eea8
runekaagaard <rumi.kg@gmail.com>
2011-08-14 13:14:40 +0200

    Updating docs.

--- chano/Chano.php
+++ chano/Chano.php
@@ -934,9 +934,18 @@ class Chano implements Iterator, ArrayAccess {
     }
 
     /**
+     * Escapes characters for use in JavaScript strings. This does *not* make
+     * the string safe for use in HTML, but does protect you from syntax errors
+     * when using templates to generate JavaScript/JSON.
      *
+     * For example::
+     *
+     *     <?$item->value->escapejs()?>
+     *
+     * If ``value`` is ``"testing\r\njavascript \'string" <b>escaping</b>"``,
+     * the output will be
+     * ``"testing\\u000D\\u000Ajavascript \\u0027string\\u0022 \\u003Cb\\u003Eescaping\\u003C/b\\u003E"``.
      * 
-     * @param string $format
      * @chanotype filter
      * @return Chano instance
      */
--- docs/reference.rst
+++ docs/reference.rst
@@ -526,4 +526,24 @@ Arguments
 *Returns*
   ``Chano instance``
 
+.. _escapejs:
+
+escapejs()
+++++++++++
+
+Escapes characters for use in JavaScript strings. This does *not* make
+the string safe for use in HTML, but does protect you from syntax errors
+when using templates to generate JavaScript/JSON.
+
+For example::
+
+    <?$item->value->escapejs()?>
+
+If ``value`` is ``"testing\r\njavascript \'string" <b>escaping</b>"``,
+the output will be
+``"testing\\u000D\\u000Ajavascript \\u0027string\\u0022 \\u003Cb\\u003Eescaping\\u003C/b\\u003E"``.
+
+*Returns*
+  ``Chano instance``
+
 
