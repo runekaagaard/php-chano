5b9e9d4ab432891d148798cfccef5dff64435b5b
runekaagaard <rumi.kg@gmail.com>
2011-09-16 12:31:48 +0200

    It looks like the chano_truncate_html() workes fine for multibyte strings as it was. Will keep the Cake version verbatim form for now.

--- chano/lib/text.php
+++ chano/lib/text.php
@@ -18,8 +18,7 @@
  * @access public
  * @link http://book.cakephp.org/view/1469/Text#truncate-1625
  */
-function chano_truncate($text, $length = 100, $options = array(), $encoding) {
-    $e = $encoding;
+function chano_truncate($text, $length = 100, $options = array()) {
     $default = array(
             'ending' => '...', 'exact' => true, 'html' => false
     );
@@ -27,19 +26,19 @@ function chano_truncate($text, $length = 100, $options = array(), $encoding) {
     extract($options);
 
     if ($html) {
-        if (mb_strlen(preg_replace('/<.*?>/u', '', $text), $e) <= $length) {
+        if (mb_strlen(preg_replace('/<.*?>/', '', $text)) <= $length) {
             return $text;
         }
-        $totalLength = mb_strlen(strip_tags($ending), $e);
+        $totalLength = mb_strlen(strip_tags($ending));
         $openTags = array();
         $truncate = '';
 
-        preg_match_all('/(<\/?([\w+]+)[^>]*>)?([^<>]*)/u', $text, $tags, PREG_SET_ORDER);
+        preg_match_all('/(<\/?([\w+]+)[^>]*>)?([^<>]*)/', $text, $tags, PREG_SET_ORDER);
         foreach ($tags as $tag) {
             if (!preg_match('/img|br|input|hr|area|base|basefont|col|frame|isindex|link|meta|param/s', $tag[2])) {
-                if (preg_match('/<[\w]+[^>]*>/us', $tag[0])) {
+                if (preg_match('/<[\w]+[^>]*>/s', $tag[0])) {
                     array_unshift($openTags, $tag[2]);
-                } else if (preg_match('/<\/([\w]+)[^>]*>/us', $tag[0], $closeTag)) {
+                } else if (preg_match('/<\/([\w]+)[^>]*>/s', $tag[0], $closeTag)) {
                     $pos = array_search($closeTag[1], $openTags);
                     if ($pos !== false) {
                         array_splice($openTags, $pos, 1);
@@ -48,22 +47,22 @@ function chano_truncate($text, $length = 100, $options = array(), $encoding) {
             }
             $truncate .= $tag[1];
 
-            $contentLength = mb_strlen(preg_replace('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/ui', ' ', $tag[3]), $e);
+            $contentLength = mb_strlen(preg_replace('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', ' ', $tag[3]));
             if ($contentLength + $totalLength > $length) {
                 $left = $length - $totalLength;
                 $entitiesLength = 0;
-                if (preg_match_all('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/ui', $tag[3], $entities, PREG_OFFSET_CAPTURE)) {
+                if (preg_match_all('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', $tag[3], $entities, PREG_OFFSET_CAPTURE)) {
                     foreach ($entities[0] as $entity) {
                         if ($entity[1] + 1 - $entitiesLength <= $left) {
                             $left--;
-                            $entitiesLength += mb_strlen($entity[0], $e);
+                            $entitiesLength += mb_strlen($entity[0]);
                         } else {
                             break;
                         }
                     }
                 }
 
-                $truncate .= mb_substr($tag[3], 0 , $left + $entitiesLength, $e);
+                $truncate .= mb_substr($tag[3], 0 , $left + $entitiesLength);
                 break;
             } else {
                 $truncate .= $tag[3];
@@ -74,18 +73,18 @@ function chano_truncate($text, $length = 100, $options = array(), $encoding) {
             }
         }
     } else {
-        if (mb_strlen($text, $e) <= $length) {
+        if (mb_strlen($text) <= $length) {
             return $text;
         } else {
-            $truncate = mb_substr($text, 0, $length - mb_strlen($ending, $e), $e);
+            $truncate = mb_substr($text, 0, $length - mb_strlen($ending));
         }
     }
     if (!$exact) {
-        $spacepos = mb_strrpos($truncate, ' ', null, $e);
+        $spacepos = mb_strrpos($truncate, ' ');
         if (isset($spacepos)) {
             if ($html) {
-                $bits = mb_substr($truncate, $spacepos, null,  $e);
-                preg_match_all('/<\/([a-z]+)>/u', $bits, $droppedTags, PREG_SET_ORDER);
+                $bits = mb_substr($truncate, $spacepos);
+                preg_match_all('/<\/([a-z]+)>/', $bits, $droppedTags, PREG_SET_ORDER);
                 if (!empty($droppedTags)) {
                     foreach ($droppedTags as $closingTag) {
                         if (!in_array($closingTag[1], $openTags)) {
@@ -94,7 +93,7 @@ function chano_truncate($text, $length = 100, $options = array(), $encoding) {
                     }
                 }
             }
-            $truncate = mb_substr($truncate, 0, $spacepos, $e);
+            $truncate = mb_substr($truncate, 0, $spacepos);
         }
     }
     $truncate .= $ending;
--- tests/filters/149_truncatewords_html.phpt
+++ tests/filters/149_truncatewords_html.phpt
@@ -3,7 +3,7 @@ A generated testfile for the "truncatewordshtml" filter.
 --FILE--
 <?php
 include dirname(__FILE__) . '/../../chano/Chano.php';
-$items = new Chano(array(array('input' => '<p>one <a href="#">two - three <br>four</a> five</p>')));
+$items = new Chano(array(array('input' => '<p>oneøæå <a href="#">twoøæå - three <br>fourøæå</a> fiveøæå</p>')));
 foreach ($items as $i) echo $i->input->truncatewordshtml(2);
 --EXPECT--
-<p>one <a href="#">two ...</a></p>
+<p>oneøæå <a href="#">twoøæå ...</a></p>
