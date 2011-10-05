544e9294070832198544b6747e81bd39078f76d6
runekaagaard <rumi.kg@gmail.com>
2011-10-05 22:08:33 +0200

    Adding an idea for template inheritance, that looks very promising.

---
+++ ideas/blog.php
@@ -0,0 +1,14 @@
+<?Chano::blocks()?>
+
+	<?Chano::block('title')?>My amazing blog<?Chano::endblock()?>
+
+	<?Chano::block('content')?>
+		<h2>Entry one</h2>
+		<p>This is my first entry.</p>
+
+		<h2>Entry two</h2>
+		<p>This is my second entry.</p>
+	<?Chano::endblock()?>
+
+<?Chano::endblocks()?>
+<?require 'base.php'?>
---
+++ ideas/inheritance.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+Below an idea, showing a working example of how template inheritance
+in pure non compiled PHP could be achieved. This example performs the
+same kind of inheritance described at https://docs.djangoproject.com/en/dev/topics/templates/#template-inheritance.
+
+It should be possible to implement the "super" feature as well.
+*/
+error_reporting(E_ALL|E_STRICT);
+ini_set('display_errors', 1);
+
+class Chano {
+	private static $active_block = null;
+	private static $blocks = array();
+
+	private static $inside_blocks = false;
+
+	static function blocks() {
+		self::$inside_blocks = true;
+		ob_start();
+	}
+
+	static function endblocks() {
+		self::$inside_blocks = false;
+		ob_end_clean();
+	}
+
+	static function block($name) {
+		ob_start();
+		self::$active_block = $name;
+	}
+
+	static function endblock() {
+		if (self::$inside_blocks) {
+			if (empty(self::$blocks[self::$active_block]))
+				self::$blocks[self::$active_block] = ob_get_clean();
+		} else {
+			if (!empty(self::$blocks[self::$active_block])) {
+				ob_end_clean();
+				echo self::$blocks[self::$active_block];
+			} else {
+				echo ob_get_clean();
+			}
+		}
+		
+	}
+}
+
+require 'blog.php';
